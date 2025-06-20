b064c7a41afb8401256fa65049892118a5919f5e
Isoo Pal <17isoo12@gmail.com>
2025-06-20 17:35:37 +0530

    Permutations Leetcode(46)

---
+++ permutationUsingBacktracking.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void getPerms(vector<int> &nums, int idx, vector<vector<int>> &ans){
+    if(idx == nums.size()){
+        ans.push_back({nums});
+        return;
+    }
+    for(int i=idx; i<nums.size(); i++){
+        swap(nums[i], nums[idx]);
+        getPerms(nums, idx+1, ans);
+        swap(nums[i], nums[idx]);   //Backtracking step.
+    }
+}
+
+int main(){
+    vector<int> nums = {1,2,3};
+    vector<vector<int>> ans;
+    getPerms(nums, 0, ans);
+
+    for(auto &i : ans){
+        for(auto &j : i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
