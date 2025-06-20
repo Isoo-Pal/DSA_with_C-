6826081aee2f7c0968ffad148b2600a3bd8c31af
Isoo Pal <17isoo12@gmail.com>
2025-06-20 16:09:36 +0530

    Subsets Leetcode Problem(78)

--- subsetsUsingRecursion.cpp
+++ subsetsUsingRecursion.cpp
@@ -2,12 +2,36 @@
 #include<vector>
 using namespace std;
 
+void getallSubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allsubsets){
+    int n = nums.size();
+    if(i == n){
+        allsubsets.push_back({ans});
+        return;
+    }
+
+    //include
+    ans.push_back(nums[i]);
+    getallSubsets(nums, ans, i+1, allsubsets);
+
+    //exclude
+    ans.pop_back();
+    getallSubsets(nums, ans, i+1, allsubsets);
+
+}
+
 int main(){
     vector<int> nums = {1,2,3};
     vector<vector<int>> allsubsets;
     vector<int> ans;
 
-    vector<vector<int>> res = getallSubsets(nums, ans, 0, allsubsets);
+    getallSubsets(nums, ans, 0, allsubsets);
+
+    for(auto &i : allsubsets){
+        for(auto &j : i){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 
 
 }
